4ac84df5bda387f84ddade4ba52f76c96f9880c5
Tomoya_Sunami <sunami@imc.cce.i.kyoto-u.ac.jp>
2019-12-15 09:43:12 +0900

    [improve] initialize

--- initialize.py
+++ initialize.py
@@ -5,14 +5,16 @@ def main():
     print(cmd)
     subprocess.call(cmd.split(" "))
 
-    #cmd = ": > test_detected_series.txt"
-    #print(cmd)
-    #subprocess.call(cmd.split(" "))
-
-
-    #cmd = ": > test_prediction_series.txt"
-    #print(cmd)
-    #subprocess.call(cmd.split(" "))
+    with open("observed_sequence.txt", mode='w') as f:
+        str_w = ""
+        f.write(str_w)
+    with open("decoded_state_sequence.txt", mode='w') as f:
+        str_w = ""
+        f.write(str_w)
+    with open("mode_observed_sequence.txt", mode='w') as f:
+        str_w = ""
+        f.write(str_w)
+    print("clean observed_sequence.txt, decoded_state_sequence.txt, mode_observed_sequence.txt")
 
     cmd = "cp backup_iperf_flag.txt iperf_flag.txt"
     print(cmd)

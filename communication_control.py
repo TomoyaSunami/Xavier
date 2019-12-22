2292f3441245763459ff0b07b965e704f06499f2
Xavier1 <sunami@imc.cce.i.kyoto-u.ac.jp>
2019-12-22 17:13:21 +0900

    syn

---
+++ communication_control.py
@@ -0,0 +1,165 @@
+import time
+import numpy as np
+import joblib
+import subprocess
+import logging
+
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+class IPERF:
+    def __init__(self,ip):
+        self.ip = ip
+        self.port = ""
+        self.bandwidth = ""
+        self.pid = None
+        self.state = "OFF"
+        self.proc = None
+    def set_port(self,port):
+        self.port = " -p " + str(port)
+    def set_bandwidth(self,bandwidth):
+        self.bandwidth = " -b " + bandwidth
+    def start(self):
+        if self.state == "OFF":
+            cmd = "iperf -u -t 60 -c " + self.ip + self.port + self.bandwidth
+            proc = subprocess.Popen(cmd.split(" "))
+            chi_pid = proc.pid
+            print("proc pid: {}".format(chi_pid))
+            self.pid = chi_pid
+            self.state = "ON"
+            self.proc = proc
+    def kill(self):
+        if self.state == "ON":
+            #print("target pid: {}".format(self.pid))
+            #cmd = "kill -9 " + str(self.pid)
+            #proc = subprocess.Popen(cmd.split(" "))
+            self.proc.kill()
+            
+            self.state = "OFF"
+            
+
+def load(path):
+    x = ''
+    while x == '':
+        with open(path, 'r') as f:
+            x = f.read()
+    return int(x)
+
+def stock(x_array, x, window_size):
+    length = len(x_array)   
+    if length < window_size:
+        x_array = np.append(x_array,x)
+    else :
+        x_array = np.roll(x_array, -1)
+        x_array[-1] = x  
+    return x_array
+
+def hmm_estimate(model, observed_sequence, pre_estimation):
+    if len(observed_sequence) <= 1:
+        return 0
+    observed_sequence = np.array([observed_sequence]).T
+
+    logprob,state_sequence = model.decode(observed_sequence)
+    
+    if model.emissionprob_[0,0] > model.emissionprob_[0,1] and model.emissionprob_[1,0] < model.emissionprob_[1,1]:
+        estimation = 0 if state_sequence[-1] == 0 else 1
+    elif model.emissionprob_[0,0] < model.emissionprob_[0,1] and model.emissionprob_[1,0] > model.emissionprob_[1,1]:
+        estimation = 1 if state_sequence[-1] == 0 else 0
+    else:
+        estimation = pre_estimation
+
+    return estimation
+
+def mode_estimate(x_array):
+    return int(sum(x_array)/len(x_array) + 0.5)
+
+def save_sequence(x, hmm_estimation, mode_estimation):
+    with open("observed_sequence.txt", mode='a') as f:
+        str_w = str(x) + "\n"
+        f.write(str_w)
+    with open("decoded_state_sequence.txt", mode='a') as f:
+        str_w = str(hmm_estimation) + "\n"
+        f.write(str_w)
+    with open("mode_observed_sequence.txt", mode='a') as f:
+        str_w = str(mode_estimation) + "\n"
+        f.write(str_w)
+
+def main():
+    x_array1 = np.empty([1,0],int)
+    x_array2 = np.empty([1,0])
+
+    model = joblib.load("HMM_1215.pkl")
+    hmm_estimation = 0
+    """
+    #iperf1_base = IPERF("192.168.0.3")
+    #iperf1_base.set_port(5002)
+    #iperf1_base.set_bandwidth("1")
+    iperf1 = IPERF("192.168.0.3")
+    iperf1.set_port(5002)
+    iperf1.set_bandwidth("10M")
+
+    #iperf2_base = IPERF("192.168.0.3")
+    #iperf2_base.set_port(5003)
+    #iperf2_base.set_bandwidth("1")
+    iperf2 = IPERF("192.168.0.3")
+    iperf2.set_port(5003)
+    iperf2.set_bandwidth("10M")
+    """
+    iperf3_base = IPERF("192.168.0.3")
+    iperf3_base.set_port(5004)
+    iperf3_base.set_bandwidth("1")
+    iperf3 = IPERF("192.168.0.3")
+    iperf3.set_port(5004)
+    iperf3.set_bandwidth("10M")
+    
+    #iperf1_base.start()
+    #iperf2_base.start()
+    iperf3_base.start()
+    
+    time.sleep(2)
+    log.info("-------\n\n\nrunning\n\n\n------")
+    #iperf3_base.kill()
+    time.sleep(20)
+    key = 113
+    while key!=113:
+        try:
+            x = load("is_there_person.txt")
+            x_array1 = stock(x_array1, x, 10)
+            x_array2 = stock(x_array2, x, 10)
+
+            hmm_estimation = hmm_estimate(model, x_array1, hmm_estimation)
+            mode_estimation = mode_estimate(x_array2)
+            """
+            if x == 1:
+                iperf1.start()
+            else :
+                iperf1.kill()
+            """
+            """"
+            if hmm_estimation == 1:
+                iperf2.start()
+            else :
+                iperf2.kill()
+            """
+            
+            if mode_estimation == 1:
+                iperf3.start()
+            else :
+                iperf3.kill()
+        
+            #save_sequence(x, hmm_estimation, mode_estimation)
+            with open("is_there_person.txt",'w') as f:
+                f.write("")
+        except KeyboardInterrupt:
+            log.info("Keybord Interrupt")
+            break
+        
+    
+    iperf3_base.kill()
+    iperf3.kill()
+    log.info("\nfinish")
+
+
+if  __name__=="__main__":
+    main()
---
+++ generate_video.py
@@ -0,0 +1,114 @@
+"""
+generate video made of object detection images 
+
+"""
+
+import sys
+import os
+import getopt
+import numpy as np
+import cv2
+import datetime
+
+from tqdm import tqdm
+
+
+def execute_cmdline(argv):
+
+    fps = 20
+    input_path = "output"
+    output_name = "video"
+
+    help_str = 'make_output_video.py -f <fps> -i <input_path> -o <output_name>'
+    try:
+        opts, args = getopt.getopt(argv, 'f:i:o:', ['fps=', 'input=', 'output='])
+    except getopt.GetoptError as err:
+        print(str(err))
+        print(help_str)
+        sys.exit(1)
+    for opt,arg in opts:
+        if opt in ("-f", "--fps"):
+            fps = float(arg)
+        elif opt in ("-i", "--input"):
+            input_path = arg
+        elif opt in ("-o", "--output"):
+            output_name = arg
+
+    if not os.path.exists(input_path):
+        print(input_path, 'does not exist')
+        sys.exit(1)
+
+    write_video(fps, input_path, output_name)
+
+def load_images(images_folder_path):
+
+    # extract timestamp from file name
+    img_names = [f for f in os.listdir(images_folder_path) if not f.startswith(".")]
+    img_times = [s.strip(".jpg").strip("'") for s in img_names]
+    img_times = [float(s) for s in img_times]
+    img_times.sort() 
+
+    return img_times
+
+def write_video(fps, images_folder_path, video_name):
+
+    img_times = load_images(images_folder_path)
+
+    images_count = len(img_times)  
+    
+    time = float(img_times[-1]) - float(img_times[0])
+    
+    fps_avg = images_count / time
+    
+    img_list_size = int(time * fps)
+    
+    base_image = cv2.imread(os.path.join(images_folder_path, str(img_times[0])+".jpg"))
+    h, w, c = base_image.shape
+
+    
+    time_list = [img_times[0]+n/fps for n in range(img_list_size)]
+    time_diff = [0] * len(time_list)
+
+    img_dt = [datetime.datetime.fromtimestamp(n) for n in img_times]
+    dt_format = [str(dt.year)+"/"+str(dt.month)+"/"+str(dt.day)+" "+str(dt.hour)+":"+str(dt.minute)+":"+str(dt.second)+"."+str(dt.microsecond) for dt in img_dt]
+
+    os.makedirs("video", exist_ok=True)
+
+
+    fourcc = cv2.VideoWriter_fourcc("m","p","4","v")
+    video = cv2.VideoWriter(os.path.join("video",video_name+".mp4"), fourcc, fps, (w,h))
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    for i in tqdm(range(img_list_size)):
+        idx = np.abs(np.asarray(img_times) - time_list[i]).argmin() # serch the value in the list
+        time_diff[i] = img_times[idx] - time_list[i]
+        img = cv2.imread(os.path.join(images_folder_path, str(img_times[idx])+".jpg"))
+        cv2.putText(img,"{}".format(dt_format[idx]),(900,700), font, 0.75,(0,0,0),5,cv2.LINE_AA)
+        cv2.putText(img,"{}".format(dt_format[idx]),(900,700), font, 0.75,(255,255,255),2,cv2.LINE_AA)
+
+        video.write(img)
+        
+
+    video.release()
+    
+    save_discription(video_name,fps,fps_avg, img_times, time_list, time_diff)
+
+
+def save_discription(video_name, fps, fps_avg, img_times, time_list, time_diff):
+    # save the discription of the video as text file
+
+    path_w = "video/{}_description.txt".format(video_name)
+    img_dt = [datetime.datetime.fromtimestamp(n) for n in img_times]
+    dt_format = [str(dt.year)+"/"+str(dt.month)+"/"+str(dt.day)+" "+str(dt.hour)+":"+str(dt.minute)+":"+str(dt.second)+"."+str(dt.microsecond) for dt in img_dt]
+    l = ["target_fps : " + str(fps),  "fps_avg : " + str(fps_avg), "count_images : " + str(len(time_list)), "image_times : ", str(dt_format), "image_unix_times : " , str(time_list), "time_difference : " , str(time_diff)]
+
+    with open(path_w, mode='w') as f:
+        f.write('\n'.join(l))
+
+
+if __name__ == '__main__':
+
+    execute_cmdline(sys.argv[1:])
+
+
+

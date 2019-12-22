9ddf472f4e8c665186cf73ce7a86bc069b0cf538
Xavier1 <sunami@imc.cce.i.kyoto-u.ac.jp>
2019-12-22 18:03:07 +0900

    [fix] confidence_list size

--- yolo.py
+++ yolo.py
@@ -490,12 +490,13 @@ def main(cam, runtime, mat, point_cloud_mat, thresh, color_array):
             """
 
             confidence_list = [detection[1] for detection in detections if detection[0] == "person"]            
-            confidence = confidence_list[0]
+            print(confidence_list)
+            confidence = confidence_list[0] if len(confidence_list)!=0 else 0
             with open("confidence_person.txt", mode='w') as f:
-                str_w = str(x) + "\n"
+                str_w = str(confidence) + "\n"
                 f.write(str_w)
             with open("output_sequence.txt", mode='a') as f:
-                str_w = str(x) + "\n"
+                str_w = str(confidence) + "\n"
                 f.write(str_w)
 
             image = plot_bounding_box(image, detections, color_array, depth)

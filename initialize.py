23ef9d213bddc28704ba698d06ebbb4b8d235eb5
Xavier1 <sunami@imc.cce.i.kyoto-u.ac.jp>
2019-12-22 23:19:50 +0900

    [fix] initalize

--- initialize.py
+++ initialize.py
@@ -7,7 +7,7 @@ def main():
     os.mkdir(target_dir)
     print("clean output directory")
 
-    with open("is_there_person.txt", mode='w') as f:
+    with open("confidence_person.txt", mode='w') as f:
         str_w = ""
         f.write(str_w)
     with open("output_sequence.txt", mode='w') as f:
@@ -19,10 +19,10 @@ def main():
     with open("decoded_state_sequence.txt", mode='w') as f:
         str_w = ""
         f.write(str_w)
-    with open("mode_observed_sequence.txt", mode='w') as f:
+    with open("avg_observed_sequence.txt", mode='w') as f:
         str_w = ""
         f.write(str_w)
-    print("clean is_there_person.txt, output_sequence.txt observed_sequence.txt, decoded_state_sequence.txt, mode_observed_sequence.txt")
+    print("clean confidence_person.txt, output_sequence.txt observed_sequence.txt, decoded_state_sequence.txt, avg_observed_sequence.txt")
 
 
 if __name__=="__main__":
